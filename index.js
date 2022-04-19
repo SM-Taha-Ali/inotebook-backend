a88cbf8148ce637849bdda02755b152d9f1f7fe9
unknown <sskk8226@gmail.com>
2022-04-19 15:08:17 +0500

    done

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import express and connection arrow function
 const connection = require('./db_connect');
 const express = require('express')
+var cors = require('cors')
 
 
 // Connection to MongoDB
 connection();
 
+
 const app = express()
 const port = 5000
-
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
